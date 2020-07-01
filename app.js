4601de2f7f32620f98a7026f574ea720a9d2676e
YuyusanK <kurinoyuichi@gmail.com>
2020-07-01 16:58:41 -0700

    signup site

--- app.js
+++ app.js
@@ -46,6 +46,11 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.use(function(req, res, next) {
+    req.con = con;
+    next();
+});
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
