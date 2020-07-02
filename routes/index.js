a1663471c5b9ff1f549ba2a5fb2cbc9d826bb79c
Bryce Hiraoka <brycehiraoka@Bryces-MacBook-Pro.local>
2020-07-01 17:17:04 -0700

    add router thing

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,19 @@ router.get('/signup',function(req,res,next){
   res.render('signup', {title:'Express'})
 });
 
+router.post('/submitUser', function(req, res){
+  const accountInfo = {
+    email: req.body.email,
+    password: req.body.password
+  };
+  const db = req.con;
+  db.query("INSERT INTO accounts SET ?", accountInfo, function(rows, err){
+    if(err) {
+      console.log(err);
+    }else{
+      res.send("Insertion successful");
+    }
+  })
+});
+
 module.exports = router;
